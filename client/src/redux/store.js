c88d9cc6b8091b0809863e8dc210c8b62f489df1
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-02-16 00:30:39 +0600

    added footer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,16 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { restaurantsApi } from "./services/restaurantsApi";
 import { foodsApi } from "./services/foodsApi";
+import { categoriesApi } from "./services/categoriesApi";
 
 const store = configureStore({
   reducer: {
     [restaurantsApi.reducerPath]: restaurantsApi.reducer,
     [foodsApi.reducerPath]: foodsApi.reducer,
+    [categoriesApi.reducerPath]: categoriesApi.reducer
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware().concat([
       restaurantsApi.middleware,
       foodsApi.middleware,
+      categoriesApi.middleware
     ]);
   },
 });
