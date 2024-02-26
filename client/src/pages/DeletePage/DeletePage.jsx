63282d04446f44dc6a6c0a0f952059cee5dffd99
Aibyn Talgatov <a1byn.talgat05@gmail.com>
2024-02-27 02:47:54 +0600

    added delete restaurant page

--- client/src/pages/DeletePage/DeletePage.jsx
+++ client/src/pages/DeletePage/DeletePage.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
-import "./DeletePage.css"
+import "./DeletePage.css";
 import CategoryDeleteForm from "../../components/CategoryDeleteForm/CategoryDeleteForm";
+import RestaurantDeleteForm from "../../components/RestaurantDeleteForm/RestaurantDeleteForm";
 
 function DeletePage() {
   return (
     <div className="delete-page-container">
       <div className="delete-page-section">
         <h1>Delete Page</h1>
-        <Link className="link" to={"/admin"}><button>AdminPage</button></Link>
+        <Link className="link" to={"/admin"}>
+          <button>AdminPage</button>
+        </Link>
       </div>
       <div className="delete-page-components">
         <CategoryDeleteForm />
+        <RestaurantDeleteForm />
       </div>
     </div>
   );
